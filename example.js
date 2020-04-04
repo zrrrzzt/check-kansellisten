96ce2c69ed4028a2ec7da0b965404e6161d12bd0
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-04 15:23:10 +0200

    Updates example (patch)

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const fs = require('fs')
-const textClean = fs.readFileSync('test/data/text-clean.txt', 'utf-8')
-const textDirty = fs.readFileSync('test/data/text-dirty.txt', 'utf-8')
+const { readFileSync } = require('fs')
+const textClean = readFileSync('test/data/text-clean.txt', 'utf-8')
+const textDirty = readFileSync('test/data/text-dirty.txt', 'utf-8')
 const checkKansellisten = require('./index')
 
 console.log(checkKansellisten(textClean))
