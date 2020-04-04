8a13303de6afe96f415565aefce78ee046eee6c2
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-04 15:25:17 +0200

    standard --fix (patch)

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const kansellisten = require('./data/data.json')
 
 module.exports = text => {
-  let a = new Set(kansellisten)
-  let b = new Set(text.split(' '))
-  let intersection = new Set(
+  const a = new Set(kansellisten)
+  const b = new Set(text.split(' '))
+  const intersection = new Set(
     [...a].filter(x => b.has(x)))
   return [...intersection]
 }
